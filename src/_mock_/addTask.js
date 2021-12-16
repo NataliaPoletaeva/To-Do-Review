5b461d27c95fe8b3f98c6a3d3d1d6ae9eb1f2ea8
NataliaPoletaeva <natfly799@gmail.com>
2021-12-16 15:27:25 +0500

    Fix linters

--- src/_mock_/addTask.js
+++ src/_mock_/addTask.js
@@ -1,19 +1,19 @@
 import localMock from './localStorageMock.js';
 
 const addTask = (tasks, todo) => {
-    tasks.push({
-        index: tasks.length + 1,
-        description: todo,
-        completed: false,
-    });
-    localMock.setItem('todoList', JSON.stringify(tasks));
+  tasks.push({
+    index: tasks.length + 1,
+    description: todo,
+    completed: false,
+  });
+  localMock.setItem('todoList', JSON.stringify(tasks));
 };
 
 const removeTask = (todo, tasks) => {
-    const { index } = todo;
-    const filteredTasks = tasks.filter((todo) => todo.index !== index);
-    localMock.setItem('todoList', JSON.stringify(filteredTasks));
-    return filteredTasks;
-}
+  const { index } = todo;
+  const filteredTasks = tasks.filter((todo) => todo.index !== index);
+  localMock.setItem('todoList', JSON.stringify(filteredTasks));
+  return filteredTasks;
+};
 
 export { addTask, removeTask };
--- src/_tests_/addRemove.test.js
+++ src/_tests_/addRemove.test.js
@@ -1,18 +1,18 @@
 import { addTask, removeTask } from '../_mock_/addTask.js';
 
 describe('Todo list functionality', () => {
-    let tasks = [];
-    const todoStr = 'test';
-    // addTodo test
-    test('addTask', () => {
-        addTask(tasks, todoStr);
-        expect(tasks).toHaveLength(1);
-        expect(tasks[0].description).toBe('test');
-    });
+  let tasks = [];
+  const todoStr = 'test';
+  // addTodo test
+  test('addTask', () => {
+    addTask(tasks, todoStr);
+    expect(tasks).toHaveLength(1);
+    expect(tasks[0].description).toBe('test');
+  });
 
-    test('removeTask', () => {
-        const todo = tasks[0];
-        tasks = removeTask(todo, tasks);
-        expect(tasks).toHaveLength(0);
-    })
-})
+  test('removeTask', () => {
+    const todo = tasks[0];
+    tasks = removeTask(todo, tasks);
+    expect(tasks).toHaveLength(0);
+  });
+});
